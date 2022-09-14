469ff2abb4c01599e1cc898da9a59ef6d7b4b03b
sasaki <sasaking0215@outlook.jp>
2022-09-14 16:51:43 +0900

    first commit

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ConversationsViewController.swift
+//  Messenger
+//
+//  Created by Mankuki_era on 2022/09/11.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+        
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        }
+    }
+
+}
